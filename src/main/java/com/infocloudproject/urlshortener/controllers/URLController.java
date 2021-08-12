e3a243058ff774ab0612fc120d225aa9cb5ce3ab
Sourabh Sinha <sourabh.sinha@navihealth.com>
2021-08-12 14:16:04 +0530

    #28 - Fixed null response issue and removed unused imports

--- src/main/java/com/infocloudproject/urlshortener/controllers/URLController.java
+++ src/main/java/com/infocloudproject/urlshortener/controllers/URLController.java
@@ -1,6 +1,5 @@
 package com.infocloudproject.urlshortener.controllers;
 
-import java.util.Optional;
 import java.util.Set;
 
 
@@ -13,7 +12,6 @@ import com.infocloudproject.urlshortener.validators.UrlValidation;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
